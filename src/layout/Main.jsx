780cce7da129628fa105ab2a9263981cf55d8940
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-28 15:54:52 +0200

    add-attribute to card-body

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -16,10 +16,22 @@ export default function Main() {
         {actresses.map((actress) => {
           return (
             <>
-              <div className="col-4">
-                <div className="card">
-                  <img src={actress.image} alt="" />
-                  <div className="card-body">{actress.name}</div>
+              <div className="col-4 my-3">
+                <div className="card h-100 g-2 ">
+                  <img className="card-img" src={actress.image} alt="" />
+
+                  <div className="card-body ">
+                    <div className="card-title mb-3">
+                      <h1 className="fs-2 fw-bold">{actress.name}</h1>
+                    </div>
+                    <div className="card-subtitle mb-3">
+                      {actress.birth_year}
+                    </div>
+                    <div className="card-text mb-3 ">{actress.nationality}</div>
+
+                    <div className="card-text mb-3">{actress.biography}</div>
+                    <div className="card-text ">{actress.awards}</div>
+                  </div>
                 </div>
               </div>
             </>
