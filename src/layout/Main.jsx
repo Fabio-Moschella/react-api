6e20771aa2381c7b96f0db4c868c64a86a35ddae
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-28 15:37:43 +0200

    add-card-actress

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,22 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 export default function Main() {
-  useEffect(
+  const [actresses, setActresses] = useState([]);
+  useEffect(() => {
     axios.get("https://lanciweb.github.io/demo/api/actresses/").then((res) => {
-      const Response = res.data;
-      console.log(Response);
-    })
-  );
+      const response = res.data;
+      setActresses(response);
+    });
+  }, []);
   return (
     <div className="container">
       <h1>ACTRESSES</h1>
+
       <div className="row">
-        <div className="col-4">
-          <div className="card">
-            <img src="" alt="#" />
-            <div className="card-body"></div>
-          </div>
-        </div>
+        {actresses.map((actress) => {
+          return (
+            <>
+              <div className="col-4">
+                <div className="card">
+                  <img src={actress.image} alt="" />
+                  <div className="card-body">{actress.name}</div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
